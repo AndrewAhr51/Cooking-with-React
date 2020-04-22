11a9c33ba5bc775cfcbd8135d7e3a43f68404880
Andrew Ahr <andrew.ahr51@gmail.com>
2020-04-22 09:09:08 -0600

    Finished

--- src/Componenets/Recipe.js
+++ src/Componenets/Recipe.js
@@ -1,47 +1,47 @@
-import React,{ useContext, useEffect} from "react";
-import IngredientList from "./IngredientList";
-import { recipeContext} from './App'
+import React, { useContext } from 'react'
+import IngredientList from './IngredientList';
+import { RecipeContext } from './App'
 
 export default function Recipe(props) {
+  const { handleRecipeDelete, handleRecipeSelect } = useContext(RecipeContext)
   const {
     id,
     name,
     cookTime,
     servings,
     instructions,
-    ingredients,
-    handleRecipeDelete,
+    ingredients
   } = props
-
-  useEffect(() => {
-    console.log("Render")
-    return() => {
-      console.log('UnMount')
-    }
-  }, [])
-
   return (
     <div className="recipe">
       <div className="recipe__header">
-        <h3 className="recipe__title"> {name}</h3>
+        <h3 className="recipe__title">{name}</h3>
         <div>
-          <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn--danger" onClick={() => handleRecipeDelete(id)}>Delete</button>
+          <button
+            className="btn btn--primary mr-1"
+            onClick={() => handleRecipeSelect(id)}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn--danger"
+            onClick={() => handleRecipeDelete(id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
       <div className="recipe__row">
-        <span className="recipe__label">Cook Time</span>
+        <span className="recipe__label">Cook Time:</span>
         <span className="recipe__value">{cookTime}</span>
       </div>
       <div className="recipe__row">
-        <span className="recipe__label">Servings</span>
+        <span className="recipe__label">Servings:</span>
         <span className="recipe__value">{servings}</span>
       </div>
       <div className="recipe__row">
-        <span className="recipe__label">Instructions</span>
-        <div className="recipe__value recipe__instructions recipe__value--indented">
-          {instructions}
-        </div>
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value recipe__instructions recipe__value--indented">{instructions}</div>
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Ingredients:</span>
@@ -50,5 +50,5 @@ export default function Recipe(props) {
         </div>
       </div>
     </div>
-  );
+  )
 }
--- src/Componenets/RecipeList.js
+++ src/Componenets/RecipeList.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Recipe from './Recipe'
+import { RecipeContext } from './App'
 
-export default function RecipeList(props) {
-    const {recipes, handleRecipeAdd, handleRecipeDelete} = props
-    return (
-        <div className="recipe-list">
-            <div>
-            {recipes.map(recipe => {
-                return <Recipe 
-                        handleRecipeDelete={handleRecipeDelete}
-                        key={recipe.id}
-                        {...recipe}/>
-            })}
-            </div>
-            <div className="recipe-list__add-recipe-btn-container">
-                <button className="btn btn--primary" onClick={handleRecipeAdd}>Add Recipe</button>
-            </div>
-        </div>
-    )
+export default function RecipeList({ recipes }) {
+  const { handleRecipeAdd } = useContext(RecipeContext)
+
+  return (
+    <div className="recipe-list">
+      <div>
+        {recipes.map(recipe => {
+          return (
+            <Recipe key={recipe.id} {...recipe} />
+          )
+        })}
+      </div>
+      <div className="recipe-list__add-recipe-btn-container">
+        <button
+          className="btn btn--primary"
+          onClick={handleRecipeAdd}
+        >
+          Add Recipe
+        </button>
+      </div>
+    </div>
+  )
 }
